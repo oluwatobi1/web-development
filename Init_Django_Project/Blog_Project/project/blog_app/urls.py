d320b7497267b52fd5aabd5e6ad61ab613f460bf
oluwatobi1 <akintunlesetobi@gmail.com>
2020-07-20 13:51:02 +0100

    Blog-added comments section functionality:

--- Init_Django_Project/Blog_Project/project/blog_app/urls.py
+++ Init_Django_Project/Blog_Project/project/blog_app/urls.py
@@ -10,7 +10,12 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view(), name = 'post_new'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name = 'post_edit'),
     path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name = 'post_delete'),
-    path('drafts/', views.PostDraftListView.as_view(), name = 'post_draft')
+    path('drafts/', views.PostDraftListView.as_view(), name = 'post_draft'),
+    path('post/<int:pk>/comment', views.add_comments_to_post, name = 'add_comments_to_post'),
+
+    path('comment/<int:pk>/approve', views.comment_approve, name = 'comment_approve'),
+    path('comment/<int:pk>/delete', views.comment_delete, name = 'comment_delete'),
+    path('post//<int:pk>/publish', views.post_publish, name = 'post_publish'),
 
 
 ]
--- Init_Django_Project/Blog_Project/project/blog_app/views.py
+++ Init_Django_Project/Blog_Project/project/blog_app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from django.contrib.auth.decorators import login_required
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import (TemplateView, ListView,
@@ -46,3 +50,43 @@ class PostDraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(publish_date__isnull = True).order_by('created_date')
+###########################################################
+###########################################################
+###########################################################
+
+@login_required
+def add_comments_to_post(request, pk):
+    post = get_object_or_404(Post, pk= pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid:
+            comment = form.save(commit = False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk = pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog_app/comment_form.html', {'form':form})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    post.publish
+    return redirect('post_detail', pk = pk)
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk = pk)
+    comment.approve()
+    return redirect('post_detail', pk = comment.post.pk)
+
+
+@login_required
+def comment_delete(request, pk):
+    comment = get_object_or_404(Comment, pk = pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk = post_pk)
